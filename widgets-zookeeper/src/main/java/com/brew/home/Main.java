4dd6ed651d73fd497775d0be218b57b1401c2bb7
shaogz <shaogz@bokecc.com>
2022-12-23 21:04:23 +0800

    zk demo

---
+++ widgets-zookeeper/src/main/java/com/brew/home/Main.java
@@ -0,0 +1,37 @@
+package com.brew.home;
+
+import org.apache.zookeeper.CreateMode;
+import org.apache.zookeeper.KeeperException;
+import org.apache.zookeeper.ZooDefs;
+import org.apache.zookeeper.ZooKeeper;
+import org.apache.zookeeper.data.Stat;
+
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
+import static org.apache.zookeeper.AddWatchMode.PERSISTENT;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException, InterruptedException, KeeperException {
+        System.out.println("Hello world!");
+
+        ZooKeeper zk = new ZooKeeper("192.168.31.26:2181,192.168.31.26:2182,192.168.31.26:2183", 4000, (event)->{
+            System.out.println("event:::::" + event);
+        });
+        zk.addWatch("/runoob", PERSISTENT);
+        TimeUnit.SECONDS.sleep(20);
+        System.out.println(zk.getState());
+
+        //相当于用stat，并注册watcher
+        //zk.exists("", (event) ->{})
+
+        //相当于get -w /runoob
+        //zk.getData("/runoob", (event) -> {}, new Stat());
+
+//        String created = zk.create("/runoob", "0".getBytes(), ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT);
+//        System.out.println(created);
+
+    }
+
+}
