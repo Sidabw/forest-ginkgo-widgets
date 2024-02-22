eb0ffed1f24b3f4840b22b7106bdd1ea5ad6d1ee
shaogz <shaogz@bokecc.com>
2024-02-23 07:50:00 +0800

    upd

--- widgets-mockito-spring/src/test/java/com/brew/home/mockito/spring/home/spring/MyFirst2JpaTest.java
+++ widgets-mockito-spring/src/test/java/com/brew/home/mockito/spring/home/spring/MyFirst2JpaTest.java
@@ -7,25 +7,24 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.util.Optional;
 import java.util.concurrent.TimeUnit;
 
 // @SpringBootTest
+
+// 单纯的测试项目里只有MyFirst1JpaTest 和 MyFirst2JpaTest时，mvn clean package会启动几次spring项目，结论：1次，哇哦~
 @DataJpaTest
 @ExtendWith(SpringExtension.class)
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
-class MyFirst1JpaTest {
+class MyFirst2JpaTest {
 
     @Autowired
     private LiveRepository liveRepository;
 
     @Test
-    void contextLoads() throws InterruptedException {
-        //需要注意的是，虽然回滚后数据不会重新插入到数据库中，但是自增主键还是会涨上去
-        //但是毕竟「构建」是发生在测试环境，所以没问题
+    void contextLoads22222() throws InterruptedException {
         Live live = new Live();
         live.setRoomId(123);
         live.setUserId(123);
@@ -40,12 +39,12 @@ class MyFirst1JpaTest {
         System.out.println(live.getId());
 
         TimeUnit.SECONDS.sleep(10);
-        System.out.println("finished");
+        System.out.println("finished2222222");
         Optional<Live> byId1 = liveRepository.findById(live.getId());
         if (byId1.isPresent()) {
             System.out.println(byId1);
         } else {
-            System.out.println("not present");
+            System.out.println("not present2222");
         }
     }
 
