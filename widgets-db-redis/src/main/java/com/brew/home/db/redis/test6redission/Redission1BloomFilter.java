d5dd488920e9f52fa0c851dff505f29cd90d8602
shaogz <shaogz@bokecc.com>
2024-05-07 19:46:15 +0800

    add redission bloom filter demo

---
+++ widgets-db-redis/src/main/java/com/brew/home/db/redis/test6redission/Redission1BloomFilter.java
@@ -0,0 +1,31 @@
+package com.brew.home.db.redis.test6redission;
+
+import org.redisson.Redisson;
+import org.redisson.api.RBloomFilter;
+import org.redisson.api.RedissonClient;
+import org.redisson.config.Config;
+
+/**
+ * @author shaogz
+ * @since 2024/5/7 19:32
+ */
+public class Redission1BloomFilter {
+
+    public static void main(String[] args) {
+        Config config = new Config();
+        config.useSingleServer().setAddress("redis://localhost:6379");
+//        config.useSingleServer().setPassword("password");
+        // 相当于创建了redis的连接
+        RedissonClient redisson = Redisson.create(config);
+
+        RBloomFilter<String> bloomFilter = redisson.getBloomFilter("myBloomFilter");
+        //初始化,预计元素数量为100000000,期待的误差率为4%
+        bloomFilter.tryInit(100000000,0.01);
+        //将号码12345插入到布隆过滤器中
+        bloomFilter.add("12345");
+
+        System.out.println(bloomFilter.contains("123456"));//false
+        System.out.println(bloomFilter.contains("12345"));//true
+    }
+
+}
