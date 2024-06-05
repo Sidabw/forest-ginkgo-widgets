eecdeaebb1e56062c60530f080e44b173e7cbcb4
shaogz <shaogz@bokecc.com>
2024-06-05 17:24:37 +0800

    upd

--- widgets-db-redis/src/main/java/com/brew/home/db/redis/test6redission/Redisson4DistributedLock.java
+++ widgets-db-redis/src/main/java/com/brew/home/db/redis/test6redission/Redisson4DistributedLock.java
@@ -14,19 +14,10 @@ import java.util.concurrent.TimeUnit;
 public class Redisson4DistributedLock {
 
     public static void main(String[] args) throws InterruptedException {
-
-        Config config = new Config();
-        config.useSingleServer().setAddress("redis://localhost:6379");
-        // 相当于创建了redis的连接
-        RedissonClient redissonClient = Redisson.create(config);
-
-        new Thread(()-> {
-            new Thread(()->{test2(redissonClient);}).start();
-        }).start();
+//        test1();
+        test2();
 
         TimeUnit.SECONDS.sleep(40);
-
-//        test1();
     }
 
     private static void test1() {
@@ -54,22 +45,39 @@ public class Redisson4DistributedLock {
         }
     }
 
-    private static void test2(RedissonClient redissonClient) {
-        RLock lock = redissonClient.getLock("lock-test");
-        try {
-            //如果想启用看门狗机制，则不能手动指定leaseTime；不启用看门狗：lock.lock(25, TimeUnit.SECONDS);
+    private static void test2() {
+        //测试结论：必须在finally里unlock
+        //不存在：线程执行完毕了就自动unlock
+        //不存在：限制执行异常了就自动unlock
+        //测试方法：不断的看ttl
 
-            //ttl 命令会返回30，之后会变成29、28、27...21、然后再变成30
-            //也就是每10s进行一次锁续期
-            lock.lock();
-            System.out.println("lock success");
-            TimeUnit.SECONDS.sleep(5);
-            System.out.println("业务代码执行完毕");
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-        } finally {
-            System.out.println("finally 执行完毕，未手动unlock");
-        }
+        new Thread(()-> {
+            new Thread(()->{
+
+                Config config = new Config();
+                config.useSingleServer().setAddress("redis://localhost:6379");
+                // 相当于创建了redis的连接
+                RedissonClient redissonClient = Redisson.create(config);
+                RLock lock = redissonClient.getLock("lock-test");
+                try {
+                    //如果想启用看门狗机制，则不能手动指定leaseTime；不启用看门狗：lock.lock(25, TimeUnit.SECONDS);
+
+                    //ttl 命令会返回30，之后会变成29、28、27...21、然后再变成30
+                    //也就是每10s进行一次锁续期
+                    lock.lock();
+                    System.out.println("lock success");
+                    TimeUnit.SECONDS.sleep(5);
+                    System.out.println("业务代码执行完毕");
+                    throw new RuntimeException("xxx");
+                } catch (Exception e) {
+                    System.out.println(e.getMessage());
+                } finally {
+                    System.out.println("finally 执行完毕，未手动unlock");
+                }
+
+            }).start();
+        }).start();
+
     }
 
 }
